53a077c22ec46cc41c13f50d4726485cce6a8470
00009115 <81857587+ID00010851@users.noreply.github.com>
2021-04-03 23:18:39 +0500

    add operation

--- routes/dreams.js
+++ routes/dreams.js
@@ -8,15 +8,10 @@ const path = require("path");
 const root = path.dirname(require.main.filename || process.require.main.filename);
 
 //database
-const data = path.join(root, "data/dreams.json");
+const database = path.join(root, "data/dreams.json");
 
-router.get("/add", (req, res) => {
-    res.render("add-dream");
-});
-
-
-router.get("/dreams", (req, res) => {
-    fs.readFile(data, (err, data) => {
+router.get("/", (req, res) => {
+    fs.readFile(database, (err, data) => {
         if (err) throw err;
 
         const dreams = JSON.parse(data);
@@ -25,12 +20,39 @@ router.get("/dreams", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+
+  const dream = {
+    id: id(),
+    title: req.body.title,
+    detail: req.body.title
+  }
+
+  fs.readFile(database, (err, data) => {
+    if (err) throw err;
+
+    const dreams = JSON.parse(data);
+
+    dreams.push(dream);
+
+    fs.writeFile(database, JSON.stringify(dreams), (err) => {
+        if (err) throw err;
+
+        res.render("dreams", { dreams: dreams });
+    });
+  })
+})
+
+router.get("/add", (req, res) => {
+    res.render("add-dream");
+});
+
 router.get("/id", (req, res) => {
     res.render("dream");
 });
 
 function id() {
-    return "_" + Math.random().toString(36).substr(2, 9);
+    return Math.random().toString(36).substr(2, 9);
 }
 
 module.exports = router;
