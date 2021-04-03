95fcec5e6d7e1ba5bbd1d9465d51960a4e62aac8
00009115 <81857587+ID00010851@users.noreply.github.com>
2021-04-03 23:54:59 +0500

    validating inputs and showing some messages after operations

--- routes/dreams.js
+++ routes/dreams.js
@@ -21,27 +21,30 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
+    const dream = {
+        id: id(),
+        title: req.body.title,
+        detail: req.body.detail,
+    };
 
-  const dream = {
-    id: id(),
-    title: req.body.title,
-    detail: req.body.detail
-  }
+    if (dream.title.trim() == "" || dream.detail.trim() == "") {
+        res.render("add-dream", { error: true })
+    } else {
+        fs.readFile(database, (err, data) => {
+            if (err) throw err;
 
-  fs.readFile(database, (err, data) => {
-    if (err) throw err;
+            const dreams = JSON.parse(data);
 
-    const dreams = JSON.parse(data);
+            dreams.push(dream);
 
-    dreams.push(dream);
+            fs.writeFile(database, JSON.stringify(dreams), (err) => {
+                if (err) throw err;
 
-    fs.writeFile(database, JSON.stringify(dreams), (err) => {
-        if (err) throw err;
-
-        res.render("dreams", { dreams: dreams });
-    });
-  })
-})
+                res.render("dreams", { dreams: dreams, success: true });
+            });
+        });
+    }
+});
 
 router.get("/add", (req, res) => {
     res.render("add-dream");
@@ -60,18 +63,18 @@ router.get("/:id", (req, res) => {
 
 router.get("/:id/delete", (req, res) => {
     fs.readFile(database, (err, data) => {
-      if (err) throw err;
+        if (err) throw err;
 
-      const dreams = JSON.parse(data);
-      const dreamIndex = dreams.findIndex(dream => dream.id == req.body.id);
-      
-      dreams.splice(dreamIndex, 1);
-      
-      fs.writeFile(database, JSON.stringify(dreams), (err) => {
-          if (err) throw err;
+        const dreams = JSON.parse(data);
+        const dreamIndex = dreams.findIndex((dream) => dream.id == req.body.id);
 
-          res.render("dreams", { dreams: dreams });
-      });
+        dreams.splice(dreamIndex, 1);
+
+        fs.writeFile(database, JSON.stringify(dreams), (err) => {
+            if (err) throw err;
+
+            res.render("dreams", { dreams: dreams, delete_success: true });
+        });
     });
 });
 
