4e80e6810ea9e7308aa066053b5ce45d7f03afe0
00009115 <81857587+ID00010851@users.noreply.github.com>
2021-04-03 23:56:43 +0500

    REST API

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const app = express();
 //routes
 const dreams = require("./routes/dreams.js");
 
+//extra-tools
+const fs = require("fs");
+const path = require("path");
+const root = path.dirname(require.main.filename || process.require.main.filename);
+
+//database
+const database = path.join(root, "data/dreams.json");
+
 app.set("view engine", "pug");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
@@ -14,6 +22,16 @@ app.get("/", (req, res) => {
 });
 
 app.use("/dreams", dreams);
+
+app.get("/api/v1/dreams", (req, res) => {
+    fs.readFile(database, (err, data) => {
+        if (err) throw err;
+
+        const dreams = JSON.parse(data);
+
+        res.json(dreams);
+    });
+});
 
 const listener = app.listen(process.env.PORT, function () {
     console.log("Your app is listening on port " + listener.address().port);
